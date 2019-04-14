fe3e1ba9679e6692af474d5a7e979ac785120965
Yana <belofelcita@gmail.com>
2019-04-14 19:24:58 +0300

    Update 02-numbers-tasks.js

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -22,7 +22,7 @@
  *   5, 5  => 25
  */
 function getRectangleArea(width, height) {
-    return (width*height);
+    return (width * height);
 }
 
 
@@ -38,7 +38,7 @@ function getRectangleArea(width, height) {
  *   0    => 0
  */
 function getCicleCircumference(radius) {
-    return(2*Math.PI*radius);
+    return (2 * Math.PI * radius);
 }
 
 /**
@@ -54,7 +54,7 @@ function getCicleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(value1, value2) {
-    throw new Error('Not implemented');
+    return (value1/2+value2/2);
 }
 
 /**
@@ -73,7 +73,7 @@ function getAverage(value1, value2) {
  *   (-5,0) (10,-10) => 18.027756377319946
  */
 function getDistanceBetweenPoints(x1, y1, x2, y2) {
-    return (Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2)));
+    return (Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)));
 }
 
 /**
@@ -89,7 +89,7 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   5*x = 0         => 0
  */
 function getLinearEquationRoot(a, b) {
-   return(-b/a);
+    return (-b / a);
 }
 
 
@@ -111,7 +111,7 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (1,2)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-    return(Math.acos((x1*x2+y1*y2)/(Math.sqrt(Math.pow(x1,2)+Math.pow(y1,2))*Math.sqrt(Math.pow(x2,2)+Math.pow(y2,2)))));
+    return (Math.acos((x1 * x2 + y1 * y2) / (Math.sqrt(Math.pow(x1, 2) + Math.pow(y1, 2)) * Math.sqrt(Math.pow(x2, 2) + Math.pow(y2, 2)))));
 }
 
 /**
@@ -127,7 +127,7 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     0     => 0
  */
 function getLastDigit(value) {
-    return(value%10);
+    return (value % 10);
 }
 
 
@@ -159,8 +159,8 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelipidedDiagonal(a,b,c) {
-    return (Math.sqrt(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2)))
+function getParallelipidedDiagonal(a, b, c) {
+    return (Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2)))
 }
 
 /**
@@ -181,7 +181,7 @@ function getParallelipidedDiagonal(a,b,c) {
  *   1678, 3  => 2000
  */
 function roundToPowerOfTen(num, pow) {
-   return ((num/Math.pow(10,pow)).toFixed(0)*Math.pow(10,pow));
+    return ((num / Math.pow(10, pow)).toFixed(0) * Math.pow(10, pow));
 }
 
 /**
@@ -202,7 +202,16 @@ function roundToPowerOfTen(num, pow) {
  *   17 => true
  */
 function isPrime(n) {
-    throw new Error('Not implemented');
+    if (n > 2 && n % 2 == 0)
+        return false;
+    else return f(Math.floor(n / 2));
+    function f(x) {
+        if (x == 1)
+            return true;
+        if (n % x == 0)
+            return false;
+        else return f(x - 1);
+    }
 }
 
 /**
@@ -221,7 +230,7 @@ function isPrime(n) {
  *   toNumber(new Number(42), 0) => 42
  */
 function toNumber(value, def) {
-    return  ((typeof(value)=='number' || value instanceof Number || !!parseInt(value))?value:def)
+    return ((typeof (value) == 'number' || value instanceof Number || !!parseInt(value)) ? value : def)
 }
 
 module.exports = {
