585bd6d7eaa21ed276d8cb8fef1afad69d2ea6ad
felcilive <47640299+felcilive@users.noreply.github.com>
2019-04-22 21:30:20 +0300

    REPEAT

--- task/08-objects-tasks.js
+++ task/08-objects-tasks.js
@@ -58,7 +58,8 @@ function getJSON(obj) {
  *
  */
 function fromJSON(proto, json) {
-    throw new Error('Not implemented');
+	return Object.setPrototypeOf(JSON.parse(json), proto);//Object.setPrototypeOf(obj, prototype);-obj-объект, которому устанавливается пртотип, prototype-новый прототип объекта
+                                                         //obj.__proto__ = proto
 }
 
 
@@ -107,39 +108,77 @@ function fromJSON(proto, json) {
  *      )
  *  ).stringify()        =>    'div#main.container.draggable + table#data ~ tr:nth-of-type(even)   td:nth-of-type(even)'
  *
- *  For more examples see unit tests.
+ *   For more examples see unit tests.
  */
 
-const cssSelectorBuilder = {
+let cssSelectorBuilder = {//просмотреть
+
+    answer: '',
 
     element: function(value) {
-        throw new Error('Not implemented');
+        this.error(1);
+        let obj = Object.create(cssSelectorBuilder);
+        obj.i = 1;
+        obj.answer = this.answer + value;
+        return obj;
     },
 
     id: function(value) {
-        throw new Error('Not implemented');
+        this.error(2);
+        let obj = Object.create(cssSelectorBuilder);
+        obj.i = 2;
+        obj.answer = this.answer + '#' + value;
+        return obj;
     },
 
     class: function(value) {
-        throw new Error('Not implemented');
+        this.error(3);
+        let obj = Object.create(cssSelectorBuilder);
+        obj.i = 3;
+        obj.answer = this.answer + '.' + value;
+        return obj;
     },
 
     attr: function(value) {
-        throw new Error('Not implemented');
+        this.error(4);
+        let obj = Object.create(cssSelectorBuilder);
+        obj.i = 4;
+        obj.answer = this.answer + '[' + value + ']';
+        return obj;
     },
 
     pseudoClass: function(value) {
-        throw new Error('Not implemented');
+        this.error(5);
+        let obj = Object.create(cssSelectorBuilder);
+        obj.i = 5;
+        obj.answer = this.answer + ':' + value;
+        return obj;
     },
 
     pseudoElement: function(value) {
-        throw new Error('Not implemented');
+        this.error(6);
+        let obj = Object.create(cssSelectorBuilder);
+        obj.i = 6;
+        obj.answer = this.answer + '::' + value;
+        return obj;
     },
 
     combine: function(selector1, combinator, selector2) {
-        throw new Error('Not implemented');
+        let obj = Object.create(cssSelectorBuilder);
+        obj.answer = selector1.answer + ' ' + combinator + ' ' + selector2.answer;
+        return obj;
+    },
+
+    stringify: function() {
+        return this.answer;
+    },
+
+    error: function(newi) {
+        if (this.i > newi) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+        if (this.i == newi && (newi == 1 || newi == 2 || newi == 6)) throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
     },
 };
+
 
 
 module.exports = {
